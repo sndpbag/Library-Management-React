fcd142c05eea9e6d91a8301eb0e6344c4f39bd2e
sndp bag <sndpbagg@gmail.com>
2025-07-05 16:49:07 +0530

    setup react project for library mangemnt with Tailwind, typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className="text-3xl text-center font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
