86189f35f8e32ae5bad5206e73c3dccfbd86387b
sndp bag <sndpbagg@gmail.com>
2025-07-05 17:05:27 +0530

    create loading compoent

---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loading: React.FC = () => (
+  <div className="flex items-center justify-center p-8">
+    <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+    <span className="ml-2 text-gray-600">Loading...</span>
+  </div>
+);
+
+export default Loading;
